27a187623ded096e411d3a2db54582d257f5a89f
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-05 01:51:55 +0300

    Change content in app component

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,25 +10,26 @@ class App extends Component {
       <div className="app"> { this.props.children }
         <Display />
         <ButtonPanel>
-          <Button label="C" value="clear" />
+        <Button label="AC" value="clear" />
           <Button label="7" value="7" />
           <Button label="4" value="4" />
           <Button label="1" value="1" />
           <Button label="0" value="0" />
 
-          <Button label="/" value="/" />
+          <Button label="+/-" value="/" />
           <Button label="8" value="8" />
           <Button label="5" value="5" />
           <Button label="2" value="2" />
-          <Button label="." value="." />
 
-          <Button label="x" value="*" />
+          <Button label="%" value="*" />
           <Button label="9" value="9" />
           <Button label="6" value="6" />
           <Button label="3" value="3" />
-          <Button label="" value="null" />
+          <Button label="." value="fullstop" />
 
-          <Button label="-" value="-" />
+          <Button label="/" size="2" value="/" />
+          <Button label="×" size="2" value="×" />
+          <Button label="-" size="2" value="-" />
           <Button label="+" size="2" value="+" />
           <Button label="=" size="2" value="equal" />
         </ButtonPanel>
