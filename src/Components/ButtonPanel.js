84c5bda20e1ce65c5e2ca7d4669ba4b9a7c8992d
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-04 23:24:38 +0300

    Edit button component

--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class ButtonPanel extends Component {
   render() {
-    return <div className="Button-Panel"></div>;
+  return <div className="Button-Panel">{ this.props.children }</div>;
   }
 }
 export default ButtonPanel;
