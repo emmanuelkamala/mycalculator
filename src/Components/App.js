9a6c24b8dd00d8c1ef7bf877f5a5ce34297dae83
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-04 17:27:05 +0300

    Add a Button component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="app">
         <h1>My Calculator</h1>
       </header>
     </div>
