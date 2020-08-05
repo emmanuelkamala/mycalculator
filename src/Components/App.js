0fba3438183a9def9c03f338bd536948abd3446a
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-05 09:32:40 +0300

    Change app component from class based to function based

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Display from './Display';
 import Button from './Button';
 import ButtonPanel from './ButtonPanel';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app"> { this.props.children }
-        <Display />
-        <ButtonPanel>
-        <Button label="AC" value="clear" />
-          <Button label="7" value="7" />
-          <Button label="4" value="4" />
-          <Button label="1" value="1" />
-          <Button label="0" value="0" />
+const app = () => {
+  return (
+    <div className="app">
+      <Display result = '0' />
+      <ButtonPanel>
+        <Button name="AC" value="clear" />
+        <Button name="7" value="7" />
+        <Button name="4" value="4" />
+        <Button name="1" value="1" />
+        <Button name="0" value="0" />
 
-          <Button label="+/-" value="/" />
-          <Button label="8" value="8" />
-          <Button label="5" value="5" />
-          <Button label="2" value="2" />
+        <Button name="+/-" value="/" />
+        <Button name="8" value="8" />
+        <Button name="5" value="5" />
+        <Button name="2" value="2" />
+        <Button name="" value="" />
 
-          <Button label="%" value="*" />
-          <Button label="9" value="9" />
-          <Button label="6" value="6" />
-          <Button label="3" value="3" />
-          <Button label="." value="fullstop" />
+        <Button name="%" value="*" />
+        <Button name="9" value="9" />
+        <Button name="6" value="6" />
+        <Button name="3" value="3" />
+        <Button name="." value="fullstop" />
 
-          <Button label="/" size="2" value="/" />
-          <Button label="×" size="2" value="×" />
-          <Button label="-" size="2" value="-" />
-          <Button label="+" size="2" value="+" />
-          <Button label="=" size="2" value="equal" />
-        </ButtonPanel>
-      </div>
-    );
-  }
+        <Button name="/" size="2" value="/" />
+        <Button name="×" size="2" value="×" />
+        <Button name="-" size="2" value="-" />
+        <Button name="+" size="2" value="+" />
+        <Button name="=" size="2" value="equal" />
+      </ButtonPanel>
+    </div>
+  )
 }
 
-App.propTypes = { children: PropTypes.node };
-
-export default App;
+export default app;
