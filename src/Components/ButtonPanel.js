661bef33b7140a56704aa18db5e8c057d50a9636
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-05 09:37:00 +0300

    Change button panel component from class based to function based

--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class ButtonPanel extends Component {
-  render() {
-    return <div className="Button-Panel">{ this.props.children }</div>;
-  }
+const buttonpanel = (props) => {
+  return <div className="Button-Panel"> { props.children } </div>
 }
 
-ButtonPanel.propTypes = { children: PropTypes.string };
+buttonpanel.propTypes = { children: PropTypes.array };
 
-export default ButtonPanel;
+export default buttonpanel;
