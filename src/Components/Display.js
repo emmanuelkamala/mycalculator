184e1702c5fcc6a8e5f3c39cbe734362f73edc67
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-20 22:07:55 +0300

    Add Styles to the display component

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const display = (props) => {
-  return <div className="Display"> { props.result } </div>;
-}
+const Display = ({ result, operator }) => (
+  <div className="display">
+    <div className="symbol">{ operator }</div>
+    <div className="result">{ result }</div>
+  </div>
+);
 
-display.propTypes = { result: PropTypes.number };
-
-export default display;
+Display.defaultProps = {
+  result: '0',
+  operator: '=',
+};
+Display.propTypes = {
+  result: PropTypes.string,
+  operator: PropTypes.string,
+};
+export default Display;
