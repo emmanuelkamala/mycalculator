5b54dd359aad1d7fe64eaeae173aca13fab7d4f8
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-20 22:11:46 +0300

    Create button panel

--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Button from './Button';
 
-const buttonpanel = (props) => {
-  return <div className="Button-Panel"> { props.children } </div>
-}
+const ButtonPanel = () => {
+  const firstrowChars = ['AC', '+/-', '%', '/'];
+  const secondRowChars = ['7', '8', '9', 'X'];
+  const thirdRowChars = ['4', '5', '6', '-'];
+  const fourthRowChars = ['1', '2', '3', '+'];
+  const fifthRowChars = ['0', '.', '='];
+  const rows = [firstrowChars, secondRowChars, thirdRowChars, fourthRowChars, fifthRowChars];
+  return (
+    <div id="button-panel">
+      {rows.map(row => (
+        <div className="row" key={row}>
+          {row.map(char => (
+            <Button
+              key={char}
+              name={String(char)}
+              color={['+', '-', 'X', '/', '='].includes(char)}
+              wide={char === '0'}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-buttonpanel.propTypes = { children: PropTypes.array };
-
-export default buttonpanel;
+export default ButtonPanel;
