ddebb5bb35cee3ff347a2ae73e2127c4b0d76b70
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-20 23:50:08 +0300

    Build operate module

---
+++ src/logic/operate.js
@@ -0,0 +1,27 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  let outcome = '';
+  switch (operation) {
+    case '+':
+      outcome = Big(numberOne).plus(numberTwo).toString();
+      break;
+    case '-':
+      outcome = Big(numberOne).minus(numberTwo).toString();
+      break;
+    case 'X':
+      outcome = Big(numberOne).times(numberTwo).toString();
+      break;
+    case '/':
+      outcome = numberTwo === '0' ? 'Infinity' : Big(numberOne).div(numberTwo).toString();
+      break;
+    case '%':
+      outcome = Big(numberOne).div('100').times(numberTwo).toString();
+      break;
+    default:
+      break;
+  }
+  return outcome;
+};
+
+export default operate;
